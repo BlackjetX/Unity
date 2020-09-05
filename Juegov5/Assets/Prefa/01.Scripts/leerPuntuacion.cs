af289d651cce0c954056803f701164979db0a714
NixonBuritik <nixon.1701222725@ucaldas.edu.co>
2020-09-05 02:58:46 -0500

    minucias

---
+++ Juegov5/Assets/Prefa/01.Scripts/leerPuntuacion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class leerPuntuacion : MonoBehaviour
+{
+     public Text puntaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1366, 768, true);
+        puntaje.text=GuardaPuntaje.puntaje.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
